795e87c575ae6daf12a688e3d916d28d4238597a
doccooper <doccooper15@gmail.com>
2025-10-28 13:39:12 +0300

    пятый коммит сделана модель первая полностью

--- models/staging/flights/stg_flights__aircrafts.sql
+++ models/staging/flights/stg_flights__aircrafts.sql
@@ -1,4 +1,17 @@
+
+-- This query selects all columns and all rows from the aircrafts table
+-- The table is referenced using the source macro which points to the 'aircrafts' table in the 'demo_src' schema
+-- Using the source macro allows for better lineage tracking in dbt
+
+-- language=sql
+
+{{ 
+    config(materialized = 'table') 
+}}
+
 select
-    *
+    aircraft_code,
+    model,
+    "range"
 from
     {{ source('demo_src', 'aircrafts') }}
